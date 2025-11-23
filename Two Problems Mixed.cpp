4f0b95d6fbaa6c87c4ce8f6bdd958ca8240353cb
AKSHAT SHARMA <118012211+AkshatSharma5@users.noreply.github.com>
2025-11-24 00:03:41 +0530

    Update Two Problems Mixed.cpp

--- Two Problems Mixed.cpp
+++ Two Problems Mixed.cpp
@@ -324,127 +324,209 @@ Case #2
 88 99
 
 
-
-Solution : 
-#include <iostream>
-#include <cstdio>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
-#define debug(x) cout << '>' << #x << ':' << x << endl;
-const int maxn = 129;
-int white = 0, blue = 0;
 
-bool checkSame(bool arr[maxn][maxn], int sti, int stj, int size)
-{
-	bool color = arr[sti][stj];
-	for(int i = sti; i < sti + size; i++){
-		for(int j = stj; j < stj + size; j++){
-			if(arr[i][j] != color){
-				return false;
-			}
-		}
-	}
-	return true;
+int n;
+vector<vector<int>> a, pref;
+
+// ---------------------------
+// Get sum of sub-rectangle
+// ---------------------------
+int getSum(int r1, int c1, int r2, int c2) {
+    int res = pref[r2][c2];
+    if (r1 > 0) res -= pref[r1 - 1][c2];
+    if (c1 > 0) res -= pref[r2][c1 - 1];
+    if (r1 > 0 && c1 > 0) res += pref[r1 - 1][c1 - 1];
+    return res;
 }
 
-void solve(bool arr[maxn][maxn], int size, int sti, int stj)
-{
-	bool same = checkSame(arr, sti, stj, size);
-	
-	if(!same){
-		solve(arr, size / 2, sti, stj);
-		solve(arr, size / 2, sti + size/2, stj);
-		solve(arr, size / 2, sti, stj + size/2);
-		solve(arr, size / 2, sti + size/2, stj + size/2);
-	}
-	else{
-		(arr[sti][stj]) ? ++blue : ++white ;
-	}
-}
+// -------------------------------------------
+// f(r1,c1,r2,c2) → returns {whiteCount, blueCount}
+// -------------------------------------------
+pair<int,int> solveQuad(int r1, int c1, int r2, int c2) {
 
-int main()
-{
-	int test ;
-	cin >> test ;
-	for(int l = 1; l <= test; l++){
-		white = 0;
-		blue = 0;
-		int size ;
-		cin >> size;
-		bool arr[maxn][maxn];
-		for(int i = 0; i < size; i++){
-			for(int j = 0; j < size; j++){
-				cin >> arr[i][j] ;
-			}
-		}
-		solve(arr, size, 0, 0);
-		cout << "Case #" << l << endl;
-		cout << white << " " << blue << endl;
-	}
-	return 0;
-}
-// optimized approach using prefix sum of matrix
+    int area = (r2 - r1 + 1) * (c2 - c1 + 1);
+    int sum = getSum(r1, c1, r2, c2);
 
-#include <iostream>
-using namespace std;
-const int maxn = 129;
-int white = 0, blue = 0;
-void solve(int arr[maxn][maxn], int size, int si,int sj)
-{
-    if( size == 0)
-    return;
-    int sum = arr[size + si - 1][size + sj - 1];
-    if(sj - 1 >= 0)
-    sum -= arr[size + si - 1][sj - 1];
-    if(si - 1 >= 0)
-    sum -= arr[si - 1][sj + size - 1];
-    if( si - 1 >= 0 && sj - 1 >= 0)
-    sum += arr[si - 1][sj - 1];
-    if(si == 4 && sj == 1)
-    cout<<sum<<endl;
-    if(sum == 0)
-    {
-       // cout<<si<<"white"<<sj<<" "<<size<<" "<<white<<endl;
-        white++;
-        return;
+    // all blue
+    if (sum == area) {
+        return {0, 1};
     }
-    if(sum == size * size)
-    {
-     //cout<<si<<"blue"<<sj<<" "<<size<<" "<<blue<<endl;
-     blue++;
-     return;
+    // all white
+    if (sum == 0) {
+        return {1, 0};
     }
-    solve(arr, size / 2, si, sj);
-    solve(arr, size / 2,  si + size/2, sj);
-    solve(arr, size / 2, si, sj + size/2);
-    solve(arr, size / 2, si + size / 2, sj + size / 2);
+
+    // otherwise mixed → need to divide
+    int rm = (r1 + r2) / 2;
+    int cm = (c1 + c2) / 2;
+
+    auto q1 = solveQuad(r1, c1, rm, cm);         // top-left
+    auto q2 = solveQuad(r1, cm + 1, rm, c2);     // top-right
+    auto q3 = solveQuad(rm + 1, c1, r2, cm);     // bottom-left
+    auto q4 = solveQuad(rm + 1, cm + 1, r2, c2); // bottom-right
+
+    return {
+        q1.first + q2.first + q3.first + q4.first,
+        q1.second + q2.second + q3.second + q4.second
+    };
 }
+
 int main() {
-    int test ;
-	cin >> test ;
-	for(int l = 1; l <= test; l++){
-		white = 0;
-		blue = 0;
-		int size ;
-		cin >> size;
-		int arr[maxn][maxn];
-		for(int i = 0; i < size; i++){
-			for(int j = 0; j < size; j++){
-                int a;
-                cin>>a;
-                if( i == 0 && j == 0)
-                 arr[i][j] = a;
-                else if( i == 0)
-                arr[i][j] = a + arr[i][j - 1];
-                else if( j == 0)
-                arr[i][j] = a + arr[i - 1][j];
-                else
-                arr[i][j] = a + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
-			}
-		}
-		solve(arr, size, 0, 0);
-		cout << "Case #" << l << endl;
-		cout << white << " " << blue << endl;
-	}
-	return 0;
+    int T;
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        cin >> n;
+        a.assign(n, vector<int>(n,0));
+        pref.assign(n, vector<int>(n,0));
+
+        // read matrix
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cin >> a[i][j];
+            }
+        }
+
+        // build prefix sum
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                pref[i][j] = a[i][j];
+                if(i>0) pref[i][j] += pref[i-1][j];
+                if(j>0) pref[i][j] += pref[i][j-1];
+                if(i>0 && j>0) pref[i][j] -= pref[i-1][j-1];
+            }
+        }
+
+        auto ans = solveQuad(0,0,n-1,n-1);
+
+        cout << "Case #" << tc << "\n";
+        cout << ans.first << " " << ans.second << "\n";
+    }
 }
+
+
+
+// Solution : 
+// #include <iostream>
+// #include <cstdio>
+// #include <cstring>
+// using namespace std;
+// #define debug(x) cout << '>' << #x << ':' << x << endl;
+// const int maxn = 129;
+// int white = 0, blue = 0;
+
+// bool checkSame(bool arr[maxn][maxn], int sti, int stj, int size)
+// {
+// 	bool color = arr[sti][stj];
+// 	for(int i = sti; i < sti + size; i++){
+// 		for(int j = stj; j < stj + size; j++){
+// 			if(arr[i][j] != color){
+// 				return false;
+// 			}
+// 		}
+// 	}
+// 	return true;
+// }
+
+// void solve(bool arr[maxn][maxn], int size, int sti, int stj)
+// {
+// 	bool same = checkSame(arr, sti, stj, size);
+	
+// 	if(!same){
+// 		solve(arr, size / 2, sti, stj);
+// 		solve(arr, size / 2, sti + size/2, stj);
+// 		solve(arr, size / 2, sti, stj + size/2);
+// 		solve(arr, size / 2, sti + size/2, stj + size/2);
+// 	}
+// 	else{
+// 		(arr[sti][stj]) ? ++blue : ++white ;
+// 	}
+// }
+
+// int main()
+// {
+// 	int test ;
+// 	cin >> test ;
+// 	for(int l = 1; l <= test; l++){
+// 		white = 0;
+// 		blue = 0;
+// 		int size ;
+// 		cin >> size;
+// 		bool arr[maxn][maxn];
+// 		for(int i = 0; i < size; i++){
+// 			for(int j = 0; j < size; j++){
+// 				cin >> arr[i][j] ;
+// 			}
+// 		}
+// 		solve(arr, size, 0, 0);
+// 		cout << "Case #" << l << endl;
+// 		cout << white << " " << blue << endl;
+// 	}
+// 	return 0;
+// }
+// // optimized approach using prefix sum of matrix
+
+// #include <iostream>
+// using namespace std;
+// const int maxn = 129;
+// int white = 0, blue = 0;
+// void solve(int arr[maxn][maxn], int size, int si,int sj)
+// {
+//     if( size == 0)
+//     return;
+//     int sum = arr[size + si - 1][size + sj - 1];
+//     if(sj - 1 >= 0)
+//     sum -= arr[size + si - 1][sj - 1];
+//     if(si - 1 >= 0)
+//     sum -= arr[si - 1][sj + size - 1];
+//     if( si - 1 >= 0 && sj - 1 >= 0)
+//     sum += arr[si - 1][sj - 1];
+//     if(si == 4 && sj == 1)
+//     cout<<sum<<endl;
+//     if(sum == 0)
+//     {
+//        // cout<<si<<"white"<<sj<<" "<<size<<" "<<white<<endl;
+//         white++;
+//         return;
+//     }
+//     if(sum == size * size)
+//     {
+//      //cout<<si<<"blue"<<sj<<" "<<size<<" "<<blue<<endl;
+//      blue++;
+//      return;
+//     }
+//     solve(arr, size / 2, si, sj);
+//     solve(arr, size / 2,  si + size/2, sj);
+//     solve(arr, size / 2, si, sj + size/2);
+//     solve(arr, size / 2, si + size / 2, sj + size / 2);
+// }
+// int main() {
+//     int test ;
+// 	cin >> test ;
+// 	for(int l = 1; l <= test; l++){
+// 		white = 0;
+// 		blue = 0;
+// 		int size ;
+// 		cin >> size;
+// 		int arr[maxn][maxn];
+// 		for(int i = 0; i < size; i++){
+// 			for(int j = 0; j < size; j++){
+//                 int a;
+//                 cin>>a;
+//                 if( i == 0 && j == 0)
+//                  arr[i][j] = a;
+//                 else if( i == 0)
+//                 arr[i][j] = a + arr[i][j - 1];
+//                 else if( j == 0)
+//                 arr[i][j] = a + arr[i - 1][j];
+//                 else
+//                 arr[i][j] = a + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+// 			}
+// 		}
+// 		solve(arr, size, 0, 0);
+// 		cout << "Case #" << l << endl;
+// 		cout << white << " " << blue << endl;
+// 	}
+// 	return 0;
+// }
